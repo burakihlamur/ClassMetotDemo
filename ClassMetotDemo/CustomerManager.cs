e75a202ff9df585531d420b9b50366deca36327e
Kübra <Kbra@DESKTOP-DO958IB>
2022-09-04 21:53:39 +0300

    Add project files.

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    internal class CustomerManager
+    {
+        
+        public void Add (Customer customer)
+        {
+
+            Console.WriteLine();
+            Console.WriteLine("Adı:" +customer.Adi+" "+ "Soyadı:"+customer.Soyadi+" "+ "Tc:"+ customer.Id +" "+ "müşteri eklendi");
+            
+
+        }
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine();
+            Console.WriteLine(customer.Adi +" "+ customer.Soyadi+" "+ customer.Id+" "+"müşteri Silindi");
+            
+        }
+
+        
+        public void List(Customer customer)
+        {
+            Console.WriteLine();
+            Console.WriteLine(customer.Adi + " " + customer.Soyadi + " " + customer.Id);
+            
+
+
+
+        }
+
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.Adi = "Gökhan";
+            customer1.Soyadi = "Keleş";
+            customer1.Id = 460092473;
+            Customer customer2 = new Customer();
+            customer2.Adi = "Hakan";
+            customer2.Soyadi = "Keleş";
+            customer2.Id = 460092474;
+            Customer customer3 = new Customer();
+            customer3.Adi = "Halil";
+            customer3.Soyadi = "Keleş";
+            customer3.Id = 460092475;
+            Console.WriteLine("-------------------");
+            CustomerManager manager = new CustomerManager();
+            manager.Add(customer1);
+            manager.Add(customer2);
+            manager.Add(customer3);
+            Console.WriteLine("-------------------");
+            manager.Delete(customer1);
+            manager.Delete(customer2);
+            manager.Delete(customer3);
+            Console.WriteLine();
+            Console.WriteLine("müşteri listemiz");
+            
+            Console.WriteLine("-------------------");
+            manager.List(customer1);
+            manager.List(customer2);
+            manager.List(customer3);
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
